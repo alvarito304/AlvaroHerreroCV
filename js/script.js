40fc8617a8201495d62c73fc4f36819ed21a0a75
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-10-29 20:18:00 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+    var body = document.getElementById('body');
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
+}
